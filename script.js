21e8bf368fdcb6bc1e66706e658f1b30794363fc
daliakhalifa02 <daliakhalifa02@gmail.com>
2023-05-13 04:35:47 +0300

    Added donation section

--- script.js
+++ script.js
@@ -34,3 +34,100 @@ post.addEventListener("click", function(){
     document.getElementById("unordered").appendChild(li);
  
 });
+
+// const donate5 = document.getElementById("donate5");
+// const donate10 = document.getElementById("donate10");
+// const donate20 = document.getElementById("donate20");
+// const donate50 = document.getElementById("donate50");
+// const donate100 = document.getElementById("donate100");
+// const donate = document.getElementById("donate");
+// const customize = document.getElementById("customize");
+// let total = 0;
+
+// donate5.addEventListener("click", function () {
+//   total += 5;
+//   updateTotal();
+// });
+
+// donate10.addEventListener("click", function () {
+//   total += 10;
+//   updateTotal();
+// });
+
+// donate20.addEventListener("click", function () {
+//   total += 20;
+//   updateTotal();
+// });
+
+// donate50.addEventListener("click", function () {
+//   total += 50;
+//   updateTotal();
+// });
+
+// donate100.addEventListener("click", function () {
+//   total += 100;
+//   updateTotal();
+// });
+
+// customize.addEventListener("click", function(){
+//   total += customize;
+//   updateTotal();
+// })
+
+// donate.addEventListener("click", function () {
+//   const customizeValue = parseInt(customize.value);
+//   if (!isNaN(customizeValue)) {
+//     total += customizeValue;
+//     updateTotal();
+//   }
+// });
+
+// function updateTotal() {
+//   document.getElementById("total").innerHTML = "Total is: " + total;
+// }
+
+const donate5 = document.getElementById("donate5");
+const donate10 = document.getElementById("donate10");
+const donate20 = document.getElementById("donate20");
+const donate50 = document.getElementById("donate50");
+const donate100 = document.getElementById("donate100");
+const customize = document.getElementById("customize");
+const donate = document.getElementById("donate");
+let total = 0;
+
+donate5.addEventListener("click", function () {
+  total += 5;
+  updateTotal();
+});
+
+donate10.addEventListener("click", function () {
+  total += 10;
+  updateTotal();
+});
+
+donate20.addEventListener("click", function () {
+  total += 20;
+  updateTotal();
+});
+
+donate50.addEventListener("click", function () {
+  total += 50;
+  updateTotal();
+});
+
+donate100.addEventListener("click", function () {
+  total += 100;
+  updateTotal();
+});
+
+donate.addEventListener("click", function () {
+  const customizeValue = parseInt(customize.value);
+  if (!isNaN(customizeValue)) {
+    total += customizeValue;
+    updateTotal();
+  }
+});
+
+function updateTotal() {
+  document.getElementById("total").innerHTML = "A total of " + total + "$ has been donated to Beirut. Thank you! ";
+}
