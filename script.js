8ba0b0eb19edac0ea9ccd611963539954036a49a
daliakhalifa02 <daliakhalifa02@gmail.com>
2023-05-13 06:43:13 +0300

    Removed extra code

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ carousel();
 function carousel() {
   var i;
   var x = document.getElementsByClassName("mySlides");
-  var numCards = 3; // Number of cards to display at a time
+  var numCards = 3;
 
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
@@ -33,7 +33,7 @@ function carousel() {
     slideIndex = 0;
   }
 
-  setTimeout(carousel, 4000); // Change cards every 4 seconds
+  setTimeout(carousel, 4000);
 }
 
 
@@ -47,57 +47,6 @@ post.addEventListener("click", function(){
     document.getElementById("unordered").appendChild(li);
  
 });
-
-// const donate5 = document.getElementById("donate5");
-// const donate10 = document.getElementById("donate10");
-// const donate20 = document.getElementById("donate20");
-// const donate50 = document.getElementById("donate50");
-// const donate100 = document.getElementById("donate100");
-// const donate = document.getElementById("donate");
-// const customize = document.getElementById("customize");
-// let total = 0;
-
-// donate5.addEventListener("click", function () {
-//   total += 5;
-//   updateTotal();
-// });
-
-// donate10.addEventListener("click", function () {
-//   total += 10;
-//   updateTotal();
-// });
-
-// donate20.addEventListener("click", function () {
-//   total += 20;
-//   updateTotal();
-// });
-
-// donate50.addEventListener("click", function () {
-//   total += 50;
-//   updateTotal();
-// });
-
-// donate100.addEventListener("click", function () {
-//   total += 100;
-//   updateTotal();
-// });
-
-// customize.addEventListener("click", function(){
-//   total += customize;
-//   updateTotal();
-// })
-
-// donate.addEventListener("click", function () {
-//   const customizeValue = parseInt(customize.value);
-//   if (!isNaN(customizeValue)) {
-//     total += customizeValue;
-//     updateTotal();
-//   }
-// });
-
-// function updateTotal() {
-//   document.getElementById("total").innerHTML = "Total is: " + total;
-// }
 
 const donate5 = document.getElementById("donate5");
 const donate10 = document.getElementById("donate10");
